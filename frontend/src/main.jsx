7b856c7681b41c58761195368ae2e737ec7a4f54
deepanshu2001 <d.deepanshusharma12@gmail.com>
2024-09-30 18:31:01 -0700

    ffff

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 import { SocketContextProvider } from './context/SocketContext.jsx'
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import { AuthContextProvider } from './context/authContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
