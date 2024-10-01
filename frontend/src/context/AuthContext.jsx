61a4c07a08cd4fc2dbb9a602116e53dac1dcd2b5
deepanshu2001 <d.deepanshusharma12@gmail.com>
2024-09-30 18:21:51 -0700

    final

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,14 @@
-import { createContext,useContext,useState } from "react";
+import { createContext, useContext, useState } from "react";
 
-export const authContext=createContext();
-export const useAuthContext=()=>{
-    return useContext(authContext)
-}
-export const AuthContextProvider=({children})=>{
-    //becuase i want to store the information as a object
-    const [authUser,setAuthUser]=useState(JSON.parse(localStorage.getItem('chat-user'))||null);
-    return <authContext.Provider value={{authUser,setAuthUser}}>
-       {children}
-    </authContext.Provider>
-}
+export const AuthContext = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {
+	return useContext(AuthContext);
+};
+
+export const AuthContextProvider = ({ children }) => {
+	const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null);
+
+	return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>;
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/authContext.jsx'
+
 import { SocketContextProvider } from './context/SocketContext.jsx'
+import { AuthContextProvider } from './context/authContext.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
