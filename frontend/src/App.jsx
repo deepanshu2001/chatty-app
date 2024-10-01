11ed1f7d077bde0d7df4a59c918b883fbfd51d57
deepanshu2001 <d.deepanshusharma12@gmail.com>
2024-09-30 18:27:41 -0700

    FF

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import Home from './pages/home/Home'
 import { Routes,Route, Navigate } from 'react-router-dom'
-import { useAuthContext } from './context/authContext';
+import { useAuthContext } from './context/AuthContext';
 function App() {
  
  const {authUser}=useAuthContext();
